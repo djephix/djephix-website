d12188997110d6bceda35d6bf850074c9309e06c
BAHA Ephraim <jeansamist@gmail.com>
2023-12-24 08:27:30 +0100

    Styles setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,56 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        border: "hsl(var(--alice-blue-2))",
+        input: "hsl(var(--alice-blue-2))",
+        ring: "hsl(var(--azure))",
+        background: "hsl(var(--ghost-white))",
+        foreground: "hsl(var(--oxford-blue))",
+        primary: {
+          DEFAULT: "hsl(var(--azure))",
+          foreground: "hsl(var(--white))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--white))",
+          foreground: "hsl(var(--oxford-blue))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--white))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--alice-blue))",
+          foreground: "hsl(var(--delft-blue))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--alice-blue))",
+          foreground: "hsl(var(--oxford-blue))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--white))",
+          foreground: "hsl(var(--oxford-blue))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--white))",
+          foreground: "hsl(var(--oxford-blue))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
